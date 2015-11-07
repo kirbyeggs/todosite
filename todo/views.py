3f42704067e3bff0ba64f2687080622011c13e45
kirbyeggs <kirbyeggs0@gmail.com>
2015-11-07 12:27:08 -0500

    changes

--- todo/views.py
+++ todo/views.py
@@ -6,9 +6,14 @@ from .models import To_do
 
 # Create your views here.
 def index(request):
+    username = request.GET['username']
     todo_list = To_do.objects.order_by('priority')
     template = loader.get_template('todo/index.html')
     context = RequestContext(request, {
         'todo_list':todo_list,
     })
     return HttpResponse(template.render(context))
+
+def splash(request):
+    template = loader.get_template('todo/splash.html')
+    return HttpResponse(template.render())
