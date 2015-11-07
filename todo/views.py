54f1ad16e441d0698a28f3258f85613af9a89067
kirbyeggs <kirbyeggs0@gmail.com>
2015-11-07 12:58:33 -0500

    fixed

--- todo/views.py
+++ todo/views.py
@@ -7,7 +7,7 @@ from .models import To_do
 # Create your views here.
 def index(request):
     username = request.GET['username']
-    todo_list = To_do.objects.order_by('priority')
+    todo_list = (To_do.objects.filter(username = username)).order_by('priority')
     template = loader.get_template('todo/index.html')
     context = RequestContext(request, {
         'todo_list':todo_list,
