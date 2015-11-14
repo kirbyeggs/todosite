87b4ac0f7bdf0e4f093c92fa540eb75e7b2ffddc
kirbyeggs <kirbyeggs0@gmail.com>
2015-11-14 13:16:15 -0500

    fixed adding entries

--- todo/urls.py
+++ todo/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.splash, name= 'splash'),
-    url(r'index/', views.index, name='index'),
+    url(r'index/$', views.index, name='index'),
     url(r'add/', views.add, name='add'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,5 @@
+import sys
+
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.template import RequestContext, loader
@@ -14,18 +16,21 @@ def splash(request):
     return render(request, 'todo/splash.html', {'username':username})
 
 def index(request):
-    username = request.GET['username']
-    request.session['user'] = username
-    todo_list = (To_do.objects.filter(username = username)).order_by('priority')
+    if 'username' in request.GET:
+        username = request.GET['username']
+        request.session['user'] = username
+    else:
+        username = request.session['user']
+    todo_list = (To_do.objects.filter(username = request.session['user'])).order_by('priority')
     template = loader.get_template('todo/index.html')
+
 
     if request.method == 'POST':
         form = AddTodo(request.POST)
         if form.is_valid():
             priority = form.cleaned_data['priority']
             description = form.cleaned_data['description']
-            username = username
-            return HttpResponseRedirect(reverse('add'))
+            return HttpResponse(request)
     else:
         form = AddTodo()
 
@@ -37,9 +42,11 @@ def index(request):
     return HttpResponse(template.render(context))
 
 def add(request):
+    print request.session['user']
     priority = request.POST['priority']
     description = request.POST['description']
     username = request.session['user']
+    date = datetime.now()
     t = To_do.objects.create(priority = priority, description = description, username = username)
     return render(request, 'todo/add.html', {
     'priority':priority,
